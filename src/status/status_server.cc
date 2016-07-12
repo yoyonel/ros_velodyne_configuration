36ed2c8ed8acf6d99fecb70bb52e2a18ed0f4f0b
ATTY Lionel <lionel.atty@ign.fr>
2016-07-12 19:07:39 +0200

    Modification du node 'velodyne_status_server' pour rajouter le test par rapport au pattern 'Service Locator'

--- src/status/status_server.cc
+++ src/status/status_server.cc
@@ -2,10 +2,21 @@
 //
 #include <vlp16_webserver_services_status.h>
 
+#include "../webserver/servicelocator/include/test_httpclient.h"
+//extern void test(const std::string & _ip);
+//#include "../webserver/servicelocator/include/locator_httpclient.h"
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "velodyne_status_server");
+
+    // ----------------------------
+    // TODO: Service Locator test
+    // ----------------------------
+    servicelocator_test("172.20.0.191", "/cgi/status.json");
+    servicelocator_test("172.20.0.191", "/cgi/settings.json");
+    servicelocator_test("172.20.0.191", "/cgi/diag.json");
+    // ----------------------------
 
     vlp16_webserver_services::Velodyne_WebServer_Status v_ws_s;
     v_ws_s.run();
